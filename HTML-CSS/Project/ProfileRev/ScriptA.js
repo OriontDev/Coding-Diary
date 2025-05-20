a3c5381a22925e87027c158ddf0e8c26735c3256
OriontDev <madeoriontfedora@gmail.com>
2025-05-20 11:05:55 +0800

    Project Revision

---
+++ HTML-CSS/Project/ProfileRev/ScriptA.js
@@ -0,0 +1,7 @@
+function changeImage() {
+    document.getElementById("gamepic2").src = "Asset/PreviewC.png";
+}
+
+function restoreImage() {
+    document.getElementById("gamepic2").src = "Asset/Image4.png";
+}
